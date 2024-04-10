c09b5ee750a8c440fd476cae4213081f988fba4b
bonmas14 <bonmas14@gmail.com>
2024-04-10 18:54:47 +0300

    testing some arrays

--- main.c
+++ main.c
@@ -1,5 +1,30 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+#define ARRAY_SIZE 100
+
+typedef struct huffman_t {
+    uint64_t value;
+    struct huffman_t *left;
+    struct huffman_t *right;
+} huffman_t;
+
+huffman_t* huffman_init() {
+    return NULL;
+}
+
 
 int main(void) {
-    printf("init commit\n");
+    uint8_t* array = malloc(sizeof(uint8_t) * ARRAY_SIZE);
+
+    if (array == NULL)
+    {
+        printf("malloc error\n");
+        exit(0);
+    }
+
+    for (uint32_t i = 0; i < ARRAY_SIZE; i++) {
+        printf("%u %u\n", i, array[i]);
+    }
 }
